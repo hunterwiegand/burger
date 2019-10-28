956c8219a21ce2775276cf4fbc4e7ea6f7131d69
Hunter Wiegand <hunter_wiegand@yahoo.com>
2019-10-28 02:30:15 -0700

    changed from retruning 404 status in controller to returning 200 status

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -33,7 +33,8 @@ router.put("/api/burger/:id", function (req, res) {
         function (result) {
             if (result.changedRows === 0) {
                 // If no rows were changed, then the ID must not exist, so 404
-                return res.status(404).end();
+                // return res.status(404).end();
+                res.status(200).end();
             }
             res.status(200).end();
 
